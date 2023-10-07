bec3e84c6ba674ca206352b5fb9c021b5af2a9bd
mohamadnaufalazim.mf <mohamadnaufalazim.mf@petronas.com>
2023-10-07 18:15:00 +0800

    Testing gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
+require('dotenv').config();
+
+
 export default function Home() {
   return (
     <UserButton afterSignOutUrl='/' />
